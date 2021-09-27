28ee8e27f6642bb017e4698dc51cf63a3be64bb1
oquendito <86526751+oquendito@users.noreply.github.com>
2021-09-27 10:20:54 -0300

    entrega_3_

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let alumno = prompt("Ingrese su nombre");
+/*let alumno = prompt("Ingrese su nombre");
 let calificacion = prompt("Ingrese su calificacion");
 
 if(calificacion >= 1 && calificacion <= 10){
@@ -15,5 +15,22 @@ if(calificacion >= 1 && calificacion <= 10){
     alert("Calificación no disponible, vuelve en 3 días hábiles");
 }
 
-console.log(`${alumno} ${calificacion}`);
+console.log(`${alumno} ${calificacion}`);*/
+
+
+
+function generarTabla(numero){
+
+    var num = parseInt(numero);
+
+    var tabla = document.getElementById("tabla");
+
+    for(let contador = 1; contador <= 10; contador++){
+        let resultado = contador * num;
+
+        let fila = tabla.insertRow(contador - 1);
+        let celda = fila.insertCell(0);
+        celda.innerHTML = contador + " * " + num + " = " + resultado;
+    }
+}
 
